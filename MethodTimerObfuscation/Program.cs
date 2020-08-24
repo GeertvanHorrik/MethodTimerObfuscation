9987ffe595b072c34bd7d1a655e9920cdc58f490
Geert van Horrik <geert@catenalogic.com>
2020-08-24 11:49:54 +0200

    Create project without obfuscation

---
+++ MethodTimerObfuscation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MethodTimerObfuscation
+{
+    class Program
+    {
+        static async void Main(string[] args)
+        {
+            Method();
+
+            await MethodAsync();
+
+            Console.WriteLine("DONE!");
+        }
+
+        [Time]
+        static void Method()
+        {
+
+        }
+
+        [Time]
+        static async Task MethodAsync()
+        {
+
+        }
+    }
+}
